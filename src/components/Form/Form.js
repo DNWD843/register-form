305c97e5cc98b6924ec8bd932a687da19fbc8a0c
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2021-02-07 22:06:18 +0300

    2 backup

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,52 +13,54 @@ function Form() {
     <form className="form app__form">
       <div className="form__container">
         <h2 className="form__title">{FORM_TITLE}</h2>
-        <p className="form__redirect-title">{REDIRECT_TITLE}</p>
-        <button type="button" className="form__redirect-button">
-          {REDIRECT_BUTTON_TEXT}
-        </button>
-
+        <div className="form__description">
+          <p className="form__redirect-title">{REDIRECT_TITLE}</p>
+          <button type="button" className="form__redirect-button">
+            {REDIRECT_BUTTON_TEXT}
+          </button>
+        </div>
+        <div className="form__link form__link_hidden">Здесь какая-то пустая ссылка из макета</div>
         <div className="form__inputs">
-          <div className="form__field">
+          <div className="form__field form__field_type_default">
             <label className="form__input-label">Имя</label>
             <input
               type="text"
               id="name"
               name="name"
               placeholder="Введите Ваше имя"
-              className="form__input form__input_type_input"
+              className="form__input form__input_type_default"
             ></input>
-            <span className="form__input-error"></span>
+            <span className="form__input-error">Validation error message</span>
           </div>
-          <div className="form__field">
+          <div className="form__field form__field_type_default">
             <label className="form__input-label">Email</label>
             <input
               type="email"
               id="email"
               name="email"
               placeholder="Введите Ваш email"
-              className="form__input form__input_type_input"
+              className="form__input form__input_type_default"
             ></input>
-            <span className="form__input-error"></span>
+            <span className="form__input-error">Validation error message</span>
           </div>
-          <div className="form__field">
+          <div className="form__field form__field_type_default">
             <label className="form__input-label">Номер телефона</label>
             <input
               type="tel"
               id="phone"
               name="phone"
               placeholder="Введите номер телефона"
-              className="form__input form__input_type_input"
+              className="form__input form__input_type_default"
             ></input>
-            <span className="form__input-error"></span>
+            <span className="form__input-error">Validation error message</span>
           </div>
-          <div className="form__field">
+          <div className="form__field form__field_type_default">
             <label className="form__input-label">Язык</label>
             <select
               id="language-select"
               name="language-select"
               value="Язык"
-              className="form__input form__input_type_select"
+              className="form__input form__input_type_default form__input_type_select "
             >
               <option value="Язык">Язык</option>
               <option value="ru">Русский</option>
@@ -83,7 +85,11 @@ function Form() {
             </p>
           </div>
         </div>
-        <button type="submit" onClick={handleSubmit} className="form__submit-button">
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="form__submit-button form__submit-button_enabled"
+        >
           Зарегистрироваться
         </button>
       </div>
